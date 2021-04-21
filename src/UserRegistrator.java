dc3190d5751b6482eff860cffa1fcc6ee6573c78
alwa0 <a.paukova@innopolis.university>
2021-04-21 14:12:12 +0300

    small fix

--- src/UserRegistrator.java
+++ src/UserRegistrator.java
@@ -2,7 +2,7 @@ public abstract class UserRegistrator {
     private String password_hash(String pass){
         return "some hash";
     }
-    public User register(String username, String email, String pass){
-        return new User();
+    public User register(long id, String username, String email, String pass){
+        return new User(id, username, email, pass);
     }
 }
