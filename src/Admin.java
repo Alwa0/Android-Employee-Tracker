c64ebf044b4404d8f739e47076a619618f180e2b
tagir <tagir@DESKTOP-QHSACCD>
2021-04-21 14:02:38 +0300

    Getters, setters, constructors for Admin, Employee, User and GPS_location classes

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,10 @@
+import java.util.Date;
+
 public class Admin extends User{
+    public Admin(long id, String password, String username, String email) {
+        super(id, password, username, email);
+    }
+    public GPS_location view_location(long id, Date date){
+        return null;
+    }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,38 @@
-public class Employee {
+import java.awt.image.BufferedImage;
+
+public class Employee extends User{
+    public GPS_location getLocation() {
+        return location;
+    }
+
+    public void setLocation(GPS_location location) {
+        this.location = location;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    private GPS_location location;
+    private BufferedImage image;
+    private long salary;
+
+    public Employee(GPS_location location, BufferedImage image, long salary, long id, String password, String username, String email) {
+        super(id, password, username, email);
+        this.location = location;
+        this.image = image;
+        this.salary = salary;
+    }
 }
