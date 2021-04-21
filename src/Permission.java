8b61aed5695109cebdcc46f9598cde0bdc143e0b
tagir <tagir@DESKTOP-QHSACCD>
2021-04-21 14:07:08 +0300

    Permission class created

--- src/Permission.java
+++ src/Permission.java
@@ -1,2 +1,35 @@
 public class Permission {
+    private long id;
+    private String permission;
+    private boolean value;
+
+    public Permission(long id, String permission, boolean value) {
+        this.id = id;
+        this.permission = permission;
+        this.value = value;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public boolean isValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
 }
