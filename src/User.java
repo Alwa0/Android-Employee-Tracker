4ad39ae48f1b2d660f5dcc8cc2d97585b9b5a038
alwa0 <a.paukova@innopolis.university>
2021-04-21 13:36:13 +0300

    add attributes and functions to User

--- src/User.java
+++ src/User.java
@@ -1,2 +1,14 @@
 public class User {
+    private long id;
+    private String password;
+    private String username;
+    private String email;
+
+    public boolean logout(){
+        return true;
+    }
+
+    public boolean login(long id, String password){
+        return true;
+    }
 }
