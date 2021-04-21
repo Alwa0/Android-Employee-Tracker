b2546bc7824d1ec281c68261ac013b252e921a52
alwa0 <a.paukova@innopolis.university>
2021-04-21 14:35:06 +0300

    add methods to Admin

--- src/Admin.java
+++ src/Admin.java
@@ -7,4 +7,13 @@ public class Admin extends User{
     public GPS_location view_location(long id, Date date){
         return null;
     }
+    public long check_salary(long id, Date date){
+        return 0;
+    }
+    public void add_employee(User new_employee){
+
+    }
+    public void change_password(long id, String new_password){
+
+    }
 }
