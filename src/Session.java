6c10e37f3fc92f38547d5c88e3cee423a5387281
tagir <tagir@DESKTOP-QHSACCD>
2021-04-21 14:05:12 +0300

    Session class created

--- src/Session.java
+++ src/Session.java
@@ -1,2 +1,35 @@
 public class Session {
+    private long id;
+    private String token;
+    private String timestamp;
+
+    public Session(long id, String token, String timestamp) {
+        this.id = id;
+        this.token = token;
+        this.timestamp = timestamp;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
