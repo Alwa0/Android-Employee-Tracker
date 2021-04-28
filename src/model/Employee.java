43404abd8544c6c125ccf7b7315c4a08b44f8fd6
alwa0 <a.paukova@innopolis.university>
2021-04-28 18:31:59 +0300

    specialization -> teams

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,10 +1,5 @@
 package model;
 
-import specialization.BackendDeveloper;
-import specialization.FrontendDeveloper;
-import specialization.PM;
-import specialization.Specialization;
-
 import java.awt.image.BufferedImage;
 
 public class Employee extends User{
@@ -35,22 +30,11 @@ public class Employee extends User{
     public GPS_location location;
     public BufferedImage image;
     public long salary;
-    public Specialization specialization;
 
     public Employee(GPS_location location, BufferedImage image, long salary, String password, String username, String email, String specialization) {
         super(password, username, email);
         this.location = location;
         this.image = image;
         this.salary = salary;
-
-        if (specialization.equals("PM")){
-            this.specialization = new PM();
-        }
-        if (specialization.equals("Backend Developer")){
-            this.specialization = new BackendDeveloper();
-        }
-        if (specialization.equals("Frontend Developer")){
-            this.specialization = new FrontendDeveloper();
-        }
     }
 }
--- src/specialization/Specialization.java
+++
@@ -1,5 +0,0 @@
-package specialization;
-
-public interface Specialization {
-    public void work();
-}
