7d6008ced56ac630243a5ac1f9a748083c40f35d
alwa0 <a.paukova@innopolis.university>
2021-04-28 19:05:51 +0300

    add methods and attributes

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Date;
-
 public class Team extends TeamComponent {
     public TeamComponent[] components;
     public Team(String password, String username, String specialization){
