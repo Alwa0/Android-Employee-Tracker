23283ab6e19987f69282b49045ab4f49a0974f00
alwa0 <a.paukova@innopolis.university>
2021-04-28 13:45:28 +0300

    rename to camelCase

--- src/registration/UserRegistrator.java
+++ src/registration/UserRegistrator.java
@@ -4,7 +4,7 @@ import model.User;
 
 public abstract class UserRegistrator {
 
-    private String password_hash(String pass){
+    private String passwordHash(String pass){
         // Some very smart hashing algorithm, this is helping method for internal usage
         // which reflects that this registrator is responsible for CREATION so that
         // factory method pattern can be applied
