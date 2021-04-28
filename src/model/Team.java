ae02acc8a4e26b01e7bb442030434577f72c43b2
alwa0 <a.paukova@innopolis.university>
2021-04-28 19:31:40 +0300

    add ORM_Model methods to team

--- src/model/Team.java
+++ src/model/Team.java
@@ -15,17 +15,19 @@ public class Team extends TeamComponent implements ORM_Model {
     }
 
     @Override
+    public void save() {
+        // some smart sql
+    }
+
+    @Override
     public ORM_Model update(String... fields) {
-        return null;
+        // some smart sql
+        return this;
     }
 
     @Override
     public ORM_Model delete(String... fields) {
-        return null;
-    }
-
-    @Override
-    public void save() {
-
+        // some smart sql
+        return this;
     }
 }
